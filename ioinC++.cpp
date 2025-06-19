b6b164bc2ef83ea07ba93a06496ba69b9dfc0c25
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-06-19 12:24:49 +0530

    Create ioinC++.cpp

---
+++ ioinC++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int num1 , num2;
+    cout << " Enter the value of num1:\n";
+    /* insertion operator */
+    cin >> num1; /* extraction operator */
+    cout << "Enter the value of num2:\n";
+    cin >> num2;
+
+    cout<<"The sum is " << num1 + num2;
+
+    return 0;
+}
