747d0a60991c54b03d1c5d942f36119bce36d713
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-06-19 11:06:27 +0530

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,26 @@
+//C++ code for variables, types and comments.
+
+#include <iostream>
+
+using namespace std;
+int main(){
+    /* Integer
+    Float
+    Char
+    Double
+    Bool
+    */
+    int a = 10;
+    float b = -20.33;
+    char c = 'S';
+    double x = 3.14159;
+    bool d = false;
+
+    cout << "Int : "<< a;
+    cout << "\nFloat : " << b;
+    cout << "\nChar : " << c;
+    cout << "\nDouble : "<< x;
+    cout << "\nBool : " << d;
+    
+    return 0;
+}
