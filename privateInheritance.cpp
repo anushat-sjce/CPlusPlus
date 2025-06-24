e630e3039a575209ff8d97f0e4ea8d985014691b
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-06-24 13:06:29 +0530

    Create privateInheritance.cpp

---
+++ privateInheritance.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Base class
+class Employee{
+    int id;
+    float salary;
+    
+    public :
+    Employee(){
+        cout << "Default constructor" << endl;
+    }
+
+    Employee(int inpId);
+    
+    void printEmployeeDetails(){
+            id  = 1001;
+            salary = 190000;
+            cout << "Id : " << id << " salary : " << salary << endl;
+    }
+};
+
+//Derived class
+class Manager : private Employee{
+    int languageCode = 9;
+    public: 
+        Manager();
+        void getManager(){
+            printEmployeeDetails();
+        }
+};
+
+Employee :: Employee(int inpId){
+        id = inpId;
+        salary = 34.0;
+    }
+    
+Manager :: Manager(){
+        cout << "Manager constructor invoked " << endl;
+    }
+    
+int main(){
+    Employee Anusha(5);
+    Manager m1;
+   // m1.printEmployeeDetails();
+   m1.getManager();
+
+    return 0;
+}
