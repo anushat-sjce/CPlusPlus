8903d0587d324a159d1caf8a340949cecb5d8e13
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-06-27 09:37:37 +0530

    Create arrayofObjectsinitialisation.cpp

---
+++ arrayofObjectsinitialisation.cpp
@@ -0,0 +1,49 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Complex {
+    int real;
+    int imaginary;
+    
+    public :
+    void setData(int a, int b){
+        real = a;
+        imaginary = b;
+        cout << "setData called " << endl;
+    }
+    
+    void printData(void){
+        cout << "real : " << real <<  endl;
+        cout << "imaginary : " << imaginary << endl;
+    }
+};
+
+int main(){
+    Complex *ptr = new Complex;
+    ptr->setData(10, 20);
+    ptr->printData();
+    
+    Complex *arr = new Complex[3];
+    arr->setData(30, 40);
+    arr->printData();
+    
+    (arr+1)->setData(50, 60);
+    (arr+1)->printData();
+    
+  //  delete[](ptr);
+    ptr->printData(); // prints junk/garbage data if called after calling delete[]
+    arr->printData();
+  //  delete[](arr);
+    arr->printData(); // prints junk/garbage data if called after calling delete[]
+    
+    
+    (arr+1)->setData(70, 80);
+    (arr+1)->printData();
+    (arr+0)->printData();
+    (arr+2)->printData();
+    
+    return 0;
+}
