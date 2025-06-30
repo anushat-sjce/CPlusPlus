c5b0226679c289e5813d1c37e068dd6e792aa8b2
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-06-30 12:23:21 +0530

    Create file2.cpp

---
+++ file2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s1 = "Hello Anupa hello welcome to this world where every step you take is of competition.no matter how intelligent you are .skill matters.";
+    string s3 = "Welcome to world";
+    ofstream out ("Sample.txt");
+    out << s1 << endl;
+    out << s3;
+    out.close();
+
+    ifstream in("Sample.txt");
+    string s2;
+    while(in.eof() == 0){
+        getline(in, s2);
+        cout << s2 << endl;
+    } 
+    in.close();
+
+    return 0;
+}
